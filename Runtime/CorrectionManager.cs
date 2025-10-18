c371337e9618ad6a1a1a825cae03eb98546eaa47
kino1022 <yuta_fujita@outlook.com>
2025-10-18 22:20:43 +0900

    update

--- Runtime/CorrectionManager.cs
+++ Runtime/CorrectionManager.cs
@@ -1,17 +1,22 @@
 using System;
 using System.Collections.Generic;
 using ObservableCollections;
+using RinaCorrection.Definition;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 
 namespace RinaCorrection {
     [Serializable]
     public class CorrectionManager : ICorrectionManager, ICorrector {
 
+        [OdinSerialize]
+        [LabelText("補正値")]
         private ObservableList<ICorrectionValue> m_corrections = new();
         
         public IReadOnlyObservableList<ICorrectionValue> Corrections => m_corrections;
 
         public float Apply(float baseValue) {
-            return 0.0f;
+            return baseValue;
         }
 
         public void Add(ICorrectionValue value) {
--- Runtime/Value/ICorrectionValue.cs
+++ Runtime/Value/ICorrectionValue.cs
@@ -1,11 +1,12 @@
 using System;
 using R3;
+using RinaCorrection.Definition;
 
 namespace RinaCorrection {
     public interface ICorrectionValue : IDisposable {
         
         ReadOnlyReactiveProperty<float> Value { get; }
         
-        
+        ICorrectionType Type { get; }
     }
 }
